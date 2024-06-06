69b31ef1619a1563315c2f4ba871abca04313f52
Drew Ellington <32873719+interfacethegreatest@users.noreply.github.com>
2024-06-06 23:18:35 +0100

    imagefix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
      <div id={styles.centeredDiv}>
-      <h3 className="text-color-blue" id={styles.centeredText} >{session?.user?.name}</h3>
+      <h1 className="text-color-yellow" id={styles.centeredText} >{session?.user?.name}</h1>
       <img id={styles.profileImage} src={session?.user?.image!}  />
       <br />
       {
