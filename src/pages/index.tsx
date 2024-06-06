f0b0cabef714a0cf936a5f457a2388e37b601bfd
Drew Ellington <32873719+interfacethegreatest@users.noreply.github.com>
2024-06-06 23:35:35 +0100

    imagefix3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import styles from '../styles/style.module.css'
 
 export default function Home() {
   const {data: session} = useSession();
+  console.log(session)
   return (
     <>
      <div id={styles.centeredDiv}>
